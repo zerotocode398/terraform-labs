3acdeb12d51dc53224fa2815dd82fc9b4385e0d7
TianCiwang <lovewtc152652>
2026-08-18 11:44:17 +0800

    ecs 查询格式化输出

--- environments/prod/output.tf
+++ environments/prod/output.tf
@@ -1,3 +1,28 @@
 output "ali_ecs_instance" {
-    value = module.ecs.ali_instance
+    # value = module.ecs.ali_instance
+    value = [
+        for ecs in module.ecs.ali_instance :
+        {
+            id = ecs.id
+            name = ecs.name
+            private_ip = ecs.private_ip
+            public_ip = ecs.public_ip
+            eip = ecs.eip
+            status = ecs.status
+            disk = join(", ", [
+                for disk in ecs.disk_device_mappings :
+                join("~", [
+                    disk.device,
+                    "${disk.size}G"
+                ])
+            ])
+            # disk = [
+            #     for disk in ecs.disk_device_mappings :
+            #     join(", ", [
+            #         disk.device,
+            #         "${disk.size}G"
+            #     ])
+            # ]
+        }
+    ]
 }

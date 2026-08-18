8f63df535939144650aef300503717aac39fcc77
TianCiwang <lovewtc152652>
2026-08-18 14:38:53 +0800

    security_groups 格式化信息

--- environments/prod/output.tf
+++ environments/prod/output.tf
@@ -1,31 +1,38 @@
-# output "ali_ecs_instance" {
-#     # value = module.ecs.ali_instance
-#     value = [
-#         for ecs in module.ecs.ali_instance :
-#         {
-#             id = ecs.id
-#             name = ecs.name
-#             # private_ip = ecs.private_ip
-#             # public_ip = ecs.public_ip
-#             sgid = toset(ecs.security_groups)
-#             # disk = join(", ", [
-#             #     for disk in ecs.disk_device_mappings :
-#             #     join("~", [
-#             #         disk.device,
-#             #         "${disk.size}G"
-#             #     ])
-#             # ])
-#             # disk = [
-#             #     for disk in ecs.disk_device_mappings :
-#             #     join(", ", [
-#             #         disk.device,
-#             #         "${disk.size}G"
-#             #     ])
-#             # ]
-#         }
-#     ]
-# }
+output "ali_ecs_instance" {
+    # value = module.ecs.ali_instance
+    value = [
+        for ecs in module.ecs.ali_instance :
+        {
+            id = ecs.id
+            name = ecs.name
+            private_ip = ecs.private_ip
+            public_ip = ecs.public_ip
+            disk = join(", ", [
+                for disk in ecs.disk_device_mappings :
+                join("~", [
+                    disk.device,
+                    "${disk.size}G"
+                ])
+            ])
+            # disk = [
+            #     for disk in ecs.disk_device_mappings :
+            #     join(", ", [
+            #         disk.device,
+            #         "${disk.size}G"
+            #     ])
+            # ]
+        }
+    ]
+}
 
 output "ali_sg_rules" {
-    value = module.security_groups.ali_sg_rules
+    description = "security group rules"
+    value = toset(flatten([
+        for sg_id,rule_list in module.security_groups.ali_sg_rules :[
+            for rules in rule_list["rules"]:
+            {
+                rule = "${sg_id}, ${rules.port_range}, ${rules.description}"
+            }
+        ]
+    ]))
 }
